3cd532d78675a90a2d156a1fd133f823a6dfd828
cmw <cmw1002113844@gmail.com>
2021-12-23 08:31:43 +0800

    基本功能

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier', 'plugin:prettier/recommended'],
+}
---
+++ src/delay/index.ts
@@ -0,0 +1,5 @@
+const a = 1
+
+interface b {
+  name: string
+}
